0a7b7812d0f47b684b74b086737d73b897e631d1
olamine zakaria <tentzakaria@gmail.com>
2023-11-05 23:05:55 +0100

    condition & dossir

--- src/components/cover_page.vue
+++ src/components/cover_page.vue
@@ -4,6 +4,8 @@
             <div class="navbar-title">LP-ESTG</div>
             <a href="#">Formations</a>
             <a href="#">Conditions d'admission</a>
+            <a href="#">Dossier de candidateur</a>
+            <a href="#">Dates important</a>
         </div>
         <div class="banner-slide">
             <div class="banner-overlay">
@@ -70,13 +72,94 @@
         <div class="services_section layout_padding">
             <div class="container">
                 <h1 class="services_taital"><span style="color: #005596">Dossier de candidateur</span></h1>
-                <center></center>
+                <center>
+                    <div class="container-main">
+                    <h4><u>Le dossier de candidature doit être déposé en ligne:</u></h4>
+                    <br>
+                    <ul>
+                        <li>
+                            <div class="checkmark">✓</div>
+                            <span class="document">Demande manuscrite adressée au directeur de l’école.</span>
+                        </li>
+                        <li>
+                            <div class="checkmark">✓</div>
+                            <span class="document">Fiche de pré-inscription en ligne signée.</span>
+                        </li>
+                        <li>
+                            <div class="checkmark">✓</div>
+                            <span class="document">Copie de CIN.</span>
+                        </li>
+                        <li>
+                            <div class="checkmark">✓</div>
+                            <span class="document">Copie de baccalauréat (recto et verso).</span>
+                        </li>
+                        <li>
+                            <div class="checkmark">✓</div>
+                            <span class="document">Copie de diplôme ou attestation de réussite BAC+2.</span>
+                        </li>
+                        <li>
+                            <div class="checkmark">✓</div>
+                            <span class="document">Copie des relevés des notes S1, S2, S3, et S4.</span>
+                        </li>
+        </ul>
+        <p class="note">N.B: Toute information erronée entraîne l’annulation de la candidature.</p>
+    </div>
+                </center>
             </div>
         </div>
         <div class="services_section layout_padding">
             <div class="container">
-                <h1 class="services_taital"><span style="color: #005596"></span>Dates important</h1>
-                <center></center>
+                <h1 class="services_taital"><span style="color: #005596"></span>Dates importants</h1>
+                    <table>
+                        <tr>
+                            <th>Event</th>
+                            <th>Date</th>
+                        </tr>
+                         <tr>
+                            <td>Préinscription en ligne</td>
+                            <td>Du Lundi 07/08/2023 au Jeudi 31/08/2023</td>
+                        </tr>
+                        <tr>
+                            <td>Sélection et étude de dossier</td>
+                            <td>Lundi 04 au Jeudi 07 Septembre 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Affichage des résultats de la sélection</td>
+                            <td>Vendredi 08 Septembre 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Concours d'admission (Test écrit)</td>
+                            <td>Vendredi 15 et Samedi 16 Septembre 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Affichage des résultats du test écrit</td>
+                            <td>A partir du Mercredi 20 Septembre 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Entretien Oral</td>
+                            <td>Lundi 25 Septembre 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Affichage des résultats de l’entretien oral</td>
+                            <td>Mardi 26 Septembre 2023</td>
+                        </tr>
+                        <tr>
+                            <td>Inscription des candidats de la liste principale</td>
+                            <td>Vendredi 29/09/2023</td>
+                        </tr>
+                        <tr>
+                            <td>Affichage de la 1ère liste d'attente</td>
+                            <td>Vendredi 29/09/2023 à 18h30</td>
+                        </tr>
+                        <tr>
+                            <td>Inscription des candidats de la 1ère liste d'attente</td>
+                            <td>Mercredi 04/10/2023</td>
+                        </tr>
+                        <tr>
+                            <td>D'autres listes d'attentes vont s'afficher à partir du mercredi 04/10/2023 dans la limite des places disponibles</td>
+                            <td></td>
+                        </tr>
+                </table>
             </div>
         </div>
     </div>
@@ -84,7 +167,51 @@
 
 <style scoped>
 @import '../assets/global.css';
+.container-main {
+            font-family: Arial, sans-serif;
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+        }
 
+        h2 {
+            color: #005596;
+            font-size: 24px;
+        }
+
+        ul {
+            list-style: none;
+            padding: 0;
+        }
+
+        li {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            margin-bottom: 10px;
+        }
+
+        .checkmark {
+            width: 20px;
+            height: 20px;
+            border: 2px solid #005596;
+            border-radius: 50%;
+            margin-right: 10px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .document {
+            color: #333;
+        }
+
+        .note {
+            color: red;
+            font-weight: bold;
+            margin-top: 20px;
+        }
 .full-page {
     overflow: auto;
     height: 100vh;
@@ -95,7 +222,24 @@
     justify-content: center;
     flex-wrap: wrap;
 }
+table {
+    border-collapse: collapse;
+    width: 60%;
+    margin: 0 auto;
+}
+th, td {
+    border: 1px solid black;
+    padding: 8px;
+    text-align: center;
+}
 
+th {
+    background-color: #005596ea;
+}
+
+tr:nth-child(even) {
+    background-color: white;
+}
 .col-md-3 {
     width: calc(50% - 20px);
     box-sizing: border-box;
