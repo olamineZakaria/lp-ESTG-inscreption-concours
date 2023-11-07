ee1215d6e2844e81f4eca4b3064ae4d625e87028
olamine zakaria <tentzakaria@gmail.com>
2023-11-07 22:01:58 +0100

    admin

--- src/components/regestration_page.vue
+++ src/components/regestration_page.vue
@@ -1,87 +1,52 @@
 <template>
     <div class="full-page">
-      <div class="limiter">
-        <div>
-          <div class="wrap-login100">
-            <img class="logo-img" src="../assets/images/img2.png" alt="Votre image" width="350" height="250" style="padding-top: 0%;">
-            <form class="login100-form validate-form" @submit.prevent="registerUser">
-              <span class="login100-form-title p-b-43">Première visite</span>
-              <pre><label class="label-style">Créer un nouveau compte<br><u>CIN: Numéro de la carte d'identité nationale</u><br><br><br></label></pre>
-              <label>CIN</label>
-              <div class="wrap-input100" data-validate="Numéro CIN requis">
-                <input class="input100" type="text" name="cin" v-model="cin">
-              </div>
-              <label>Email</label>
-              <div class="wrap-input100" data-validate="Email valide requis: ex@abc.xyz">
-                <input class="input100" type="email" name="email" v-model="email">
-              </div>
-              <label>Mot de passe</label>
-              <div class="wrap-input100" data-validate="Mot de passe requis">
-                <input class="input100" type="password" name="password" v-model="password">
-              </div>
-              <label>Confirmez le mot de passe</label>
-              <div class="wrap-input100" data-validate="Confirmez le mot de passe">
-                <input class="input100" type="password" name="confirm-password" v-model="confirmPassword">
-              </div>
-              <div class="container-login100-form-btn">
-                <button class="login100-form-btn">Créer un compte</button>
-              </div>
-            </form>
-          </div>
+        <div class="limiter">
+            <div>
+                <div class="wrap-login100">
+                    <img class="logo-img" src="../assets/images/img2.png" alt="Votre image" width="350" height="250" style="padding-top: 0%;">
+                    <form class="login100-form validate-form">
+                        <span class="login100-form-title p-b-43">Première visite</span>
+                        <pre><label class="label-style">Créer un nouveau compte<br><u>CIN: Numéro de la carte d'identité nationale</u><br><br><br></label></pre>
+                        <label>CIN</label>
+                        <div class="wrap-input100" data-validate="Numéro CIN requis">
+                            <input class="input100" type="text" name="cin">
+                        </div>
+                        <label>Email</label>
+                        <div class="wrap-input100" data-validate="Email valide requis: ex@abc.xyz">
+                            <input class="input100" type="email" name="email">
+                        </div>
+                        <label>Mot de passe</label>
+                        <div class="wrap-input100" data-validate="Mot de passe requis">
+                            <input class="input100" type="password" name="password">
+                        </div>
+                        <label>Confirmez le mot de passe</label>
+                        <div class="wrap-input100" data-validate="Confirmez le mot de passe">
+                            <input class="input100" type="password" name="confirm-password">
+                        </div>
+                        <div class="container-login100-form-btn">
+                            <button class="login100-form-btn">Créer un compte</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        cin: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-      };
-    },
-    methods: {
-      async registerUser() {
-        if (this.password !== this.confirmPassword) {
-          // Passwords do not match, handle error or show an error message
-          return;
-        }
-  
-        try {
-          const response = await axios.post('/registration', {
-            cin: this.cin,
-            email: this.email,
-            password: this.password,
-          });
-          console.log(response.data);
-          // Handle success or show a success message
-        } catch (error) {
-          console.error(error);
-          // Handle error or show an error message
-        }
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  @import '@/assets/styles_login._fonts.css';
-  @import '@/assets/stayling.css';
-  @import '@/assets/style.css';
-  
-  .full-page {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100vh;
-      background-color: #f0f0f0; /* Set the background color you prefer */
-      overflow: hidden;
-  }
-  </style>
-  
+
+</template>
+
+
+<style scoped>
+@import '@/assets/styles_login._fonts.css';
+@import '@/assets/stayling.css';
+@import '@/assets/style.css';
+
+.full-page {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background-color: #f0f0f0; /* Set the background color you prefer */
+    overflow: hidden;
+}
+</style>
